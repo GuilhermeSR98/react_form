5f0b79f83617b6aeef3fba30649b943093218a18
Guilherme Reis <guigareisbr@gmail.com>
2022-02-10 19:41:11 -0300

    Form layout configuration

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react'
 import { Container, Typography } from '@mui/material/'
 import RegistrationForm from './Components/RegistrationForm/RegistrationForm'
 
+
+
 class App extends Component {
   render() {
     return (
       <Container maxWidth="sm">
-        <Typography variant="h1">Registration Form</Typography>
+        <Typography variant="h3" component="h1">Registration Form</Typography>
         <RegistrationForm />
       </Container>
     )
