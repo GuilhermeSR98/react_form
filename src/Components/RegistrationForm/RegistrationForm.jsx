c160f73127f0428c42c99776c7819252a95ca132
Guilherme Reis <guigareisbr@gmail.com>
2022-02-18 19:18:20 -0300

    Changing the form through the switch

--- src/Components/RegistrationForm/RegistrationForm.jsx
+++ src/Components/RegistrationForm/RegistrationForm.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PersonalData from '../PersonalData/PersonalData'
 import UserData from '../UserData/UserData'
 import DeliveryData from '../DeliveryData/DeliveryData'
+import { Typography } from '@mui/material'
 
+function RegistrationForm(CPFValidation, submitForm) {
+  const [stage, setStage] = useState(0)
+  function currentStage(stage) {
+    switch (stage) {
+      case 0:
+        return <UserData />
+      case 1:
+        return <PersonalData CPFValidation={CPFValidation} submitForm={submitForm} />
+      case 2:
+        return <DeliveryData />
+      default:
+        return (
+          <Typography variant="h3" component="h1">
+            Error
+          </Typography>
+        )
+    }
+  }
 
-function RegistrationForm( CPFValidation, submitForm) {
-  return (
-    <>
-      <PersonalData CPFValidation={CPFValidation} submitForm={submitForm} />
-      <UserData />
-      <DeliveryData />
-    </>
-    
-  )
+  return <>{currentStage(stage)}</>
 }
+
 export default RegistrationForm
