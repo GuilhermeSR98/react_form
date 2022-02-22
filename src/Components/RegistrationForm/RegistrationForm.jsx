9ac42a47be0eeb684e6966b2a861c9a37b8cc18e
Guilherme Reis <guigareisbr@gmail.com>
2022-02-22 19:48:33 -0300

    Adding Thank You and Step Counter

--- src/Components/RegistrationForm/RegistrationForm.jsx
+++ src/Components/RegistrationForm/RegistrationForm.jsx
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react'
 import PersonalData from '../PersonalData/PersonalData'
 import UserData from '../UserData/UserData'
 import DeliveryData from '../DeliveryData/DeliveryData'
+import { Step, StepLabel, Stepper, Typography } from '@mui/material'
 
 function RegistrationForm({ CPFValidation, submitForm }) {
   const [stage, setStage] = useState(0)
   const [collectedData, setCollectedData] = useState({})
   useEffect(() => {
-    console.log(">>>",collectedData)
+    if (stage === forms.length-1) {
+      submitForm(collectedData)
+    }
   })
   const forms = [
     <UserData submitForm={collectData} />,
     <PersonalData submitForm={collectData} CPFValidation={CPFValidation} />,
-    <DeliveryData submitForm={collectData} />
+    <DeliveryData submitForm={collectData} />,
+    <Typography variant='h5'>Thank You!</Typography>
   ]
 
   function collectData(data) {
@@ -23,7 +27,14 @@ function RegistrationForm({ CPFValidation, submitForm }) {
     setStage(stage + 1)
   }
 
-  return <>{forms[stage]}</>
+  return <>
+    <Stepper activeStep={stage}>
+      <Step><StepLabel>Login</StepLabel></Step>
+      <Step><StepLabel>Personal</StepLabel></Step>
+      <Step><StepLabel>Delivery</StepLabel></Step>
+      <Step><StepLabel>Finalization</StepLabel></Step>
+    </Stepper>
+    {forms[stage]}</>
 }
 
 export default RegistrationForm
